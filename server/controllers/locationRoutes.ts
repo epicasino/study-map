b473d239a169e74ef026ac72089cac9b7b0ee4fd
Ray Badua <rayjonathanbadua@gmail.com>
2023-11-05 20:08:47 -0800

    added locations crud method

--- server/controllers/locationRoutes.ts
+++ server/controllers/locationRoutes.ts
@@ -1,4 +1,25 @@
 import express from 'express';
+import prisma from '../server';
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+  if (req.query.id) {
+    try {
+      const locationId = req.query.id as string;
+      const location = await prisma.location.findFirst({
+        where: { id: parseInt(locationId) },
+      });
+      if (!location) {
+        return res.status(404).json({ message: '404: Not Found' });
+      }
+      return res.status(200).json(location);
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json(err);
+    }
+  }
+  const locations = await prisma.location.findMany({});
+  return res.status(200).json(locations);
+});
 
 export default router;
