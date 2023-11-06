9f872cdd503d5df655195fe5b165b18bc0172d3f
Ray Badua <rayjonathanbadua@gmail.com>
2023-11-05 20:45:04 -0800

    added aggregate for average rating score for locations

--- server/controllers/locationRoutes.ts
+++ server/controllers/locationRoutes.ts
@@ -6,12 +6,24 @@ router.get('/', async (req, res) => {
   if (req.query.id) {
     try {
       const locationId = req.query.id as string;
+
+      // Aggregates all reviews and returns an average review score
+      const averageRating = await prisma.review.aggregate({
+        where: { locationId: parseInt(locationId) },
+        _avg: { rating: true },
+      });
+      await prisma.location.update({
+        where: { id: parseInt(locationId) },
+        data: { rating: averageRating._avg.rating },
+      });
+
       const location = await prisma.location.findFirst({
         where: { id: parseInt(locationId) },
       });
       if (!location) {
         return res.status(404).json({ message: '404: Not Found' });
       }
+
       return res.status(200).json(location);
     } catch (err) {
       console.log(err);
